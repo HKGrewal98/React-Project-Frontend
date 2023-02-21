b53f285be3cca4a5c71bbed70a2bc1afe20eb221
PriyaMJoseph <n01468981@humbermail.ca>
2023-02-20 21:39:44 -0500

    adding initial about us changes - styling pending

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -1,10 +1,19 @@
+import React from "react";
 import {Link} from 'react-router-dom'
 
-export default function About(){
+const About = () => {
     return (
-       <div style={{color:"red", marginTop:"100px"}}>
-        <p>About</p>
-        <Link to='/ottomonMenu'>Main Page</Link>
-       </div>
-    )
-}
+        <div style={{color:"red", marginTop:"100px"}}>
+         <p>About Us</p>
+         <p>we pride ourselves on serving the freshest ingredients, handpicked by our experienced chefs, to create mouth-watering dishes that will leave you wanting more. From classic comfort food to modern twists on old favorites, we have something for everyone.</p>
+
+<p>With our easy-to-use online food ordering app, you can browse our extensive menu, place your order, and have it delivered right to your doorstep. You can also choose to pick up your order at your convenience, without having to wait in long lines.</p>
+
+<p> Our dishes are made with love and care, using only the best quality ingredients, ensuring that every bite is bursting with flavor.</p>
+
+<p>So why wait? Order online and enjoy the convenience of having your favorite dishes today!</p>
+         <Link to='/ottomonMenu'>Main Page</Link>
+        </div>
+     );
+  };
+export default About;
