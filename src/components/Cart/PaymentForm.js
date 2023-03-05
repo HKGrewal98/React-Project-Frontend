dd4c1eb321420e92ecd5cdc28b94e417530df820
PriyaMJoseph <n01468981@humbermail.ca>
2023-03-04 21:27:47 -0500

    styling changes for payment page

--- src/components/Cart/PaymentForm.js
+++ src/components/Cart/PaymentForm.js
@@ -3,172 +3,214 @@ import { useState, useContext } from "react";
 import { AddressContext } from "../../store/AddressContext";
 import mealContext from "../../store/MealItemContext";
 import { useForm } from "react-hook-form";
-import Alert from '@mui/material/Alert';
-import AlertTitle from '@mui/material/AlertTitle';
-import Stack from '@mui/material/Stack';
+import Alert from "@mui/material/Alert";
+import AlertTitle from "@mui/material/AlertTitle";
+import Stack from "@mui/material/Stack";
 import { useNavigate } from "react-router-dom";
-
+import classes from "./PaymentForm.module.css";
 
 const PaymentForm = (props) => {
- const addressCtx = useContext(AddressContext)
- const mealCtx = useContext(mealContext)
- const { register, handleSubmit, watch, formState: { errors } , reset} = useForm({
-    mode:"onSubmit",
-		reValidateMode:"onChange"
- });
- const [hasError,setHasError] = useState(false)
- const [message,setMessage] = useState(null)
- const [orderSubmitted,setOrderSubmitted] = useState(false)
- const navigate = useNavigate();
+  const addressCtx = useContext(AddressContext);
+  const mealCtx = useContext(mealContext);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+    reset,
+  } = useForm({
+    mode: "onSubmit",
+    reValidateMode: "onChange",
+  });
+  const [hasError, setHasError] = useState(false);
+  const [message, setMessage] = useState(null);
+  const [orderSubmitted, setOrderSubmitted] = useState(false);
+  const navigate = useNavigate();
 
- const onSubmitHandler = async (data,event) => {
-   event.preventDefault()
-   // Getting Order Data from the respective contexts.
-   const token = mealCtx.jwt
-   const orderAddress = addressCtx.getOrderAddress()
-   const userOrder = mealCtx.cartItems
-   const totalAmount = mealCtx.totalAmount
-   const user = mealCtx.getUser()
+  const onSubmitHandler = async (data, event) => {
+    event.preventDefault();
+    // Getting Order Data from the respective contexts.
+    const token = mealCtx.jwt;
+    const orderAddress = addressCtx.getOrderAddress();
+    const userOrder = mealCtx.cartItems;
+    const totalAmount = mealCtx.totalAmount;
+    const user = mealCtx.getUser();
 
-  const body={
-    user:user,
-    orderAddress:orderAddress,
-    userOrder:userOrder,
-    totalAmount:totalAmount,
-    payment:{
-      name:data.name,
-      cardNumber:data.cardNumber,
-      cvv:data.cvv,
-      expiry:data.expiry,
-      paymentMode:data.paymentMode
+    const body = {
+      user: user,
+      orderAddress: orderAddress,
+      userOrder: userOrder,
+      totalAmount: totalAmount,
+      payment: {
+        name: data.name,
+        cardNumber: data.cardNumber,
+        cvv: data.cvv,
+        expiry: data.expiry,
+        paymentMode: data.paymentMode,
+      },
+    };
+    console.log(body);
+
+    const response = await fetch("http://localhost:8080/order", {
+      method: "post",
+      body: JSON.stringify(body),
+      headers: {
+        "Content-type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        Accept: "application/json",
+        Authorization: token,
+      },
+    });
+
+    const result = await response.json();
+    console.log(result);
+    setOrderSubmitted(true);
+    if (response.ok) {
+      setMessage(result.message);
+      setTimeout(() => {
+        mealCtx.clearCart();
+        navigate("/ottomonMenu");
+      }, 3000);
+    } else {
+      setMessage(result.message);
+      setHasError(true);
     }
-  }
-  console.log(body)
-  
-  const response = await fetch('http://localhost:8080/order',{
-    method:'post',
-    body:JSON.stringify(body),
-    headers:{
-      "Content-type":"application/json",
-			'Access-Control-Allow-Origin':'*',
-			"Accept": 'application/json',
-      "Authorization":token
-    }
-  })
 
-  const result = await response.json()
-  console.log(result)
-  setOrderSubmitted(true)
-  if(response.ok){
-    setMessage(result.message)
-    setTimeout(()=>{
-         mealCtx.clearCart()
-         navigate('/ottomonMenu')
-    },3000)
-  }else{
-    setMessage(result.message)
-    setHasError(true)
-  }
+    reset();
+  };
 
-  reset()
-
- }
-
-const closeHandler = (e) => {
-  setOrderSubmitted(false)
-  setHasError(false)
-  setMessage(null)
-}
+  const closeHandler = (e) => {
+    setOrderSubmitted(false);
+    setHasError(false);
+    setMessage(null);
+  };
 
   return (
     <React.Fragment>
-          {orderSubmitted &&  <Stack sx={{ width: '100%' }} spacing={2}>
-                                  <Alert onClose={closeHandler} severity={hasError?"error":"success"}>
-                                  <AlertTitle>{hasError?"Error":"Success"}</AlertTitle>
-                                     <strong>{message}</strong>
-                                     {!hasError && <><br/><br/><b>Redirecting to the Home Page........</b></>}
-                                  </Alert>
-                              </Stack>}
+      {orderSubmitted && (
+        <Stack sx={{ width: "100%" }} spacing={2}>
+          <Alert
+            onClose={closeHandler}
+            severity={hasError ? "error" : "success"}
+          >
+            <AlertTitle>{hasError ? "Error" : "Success"}</AlertTitle>
+            <strong>{message}</strong>
+            {!hasError && (
+              <>
+                <br />
+                <br />
+                <b>Redirecting to the Home Page........</b>
+              </>
+            )}
+          </Alert>
+        </Stack>
+      )}
 
-        <form onSubmit={handleSubmit(onSubmitHandler)}>
-          <div>
-            <label htmlFor="name">Name:</label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              {...register("name",{required:"Name is Required",pattern:{
-                value:/^[A-Za-z\s]*$/,
-                message:"Name must contain only the alphabatic letters."
-              },minLength:{
-                value:4,
-                message:"Name must be at least 4 letters long\]';/"
-              }})}
-            />
-            <p style={{color:"yellow",fontSize:"10px"}}>{errors.name?.message}</p>
-          </div>
-          <div>
-            <label htmlFor="cardNumber">Card Number:</label>
-            <input
-              type="text"
-              id="cardNumber"
-              name="cardNumber"
-              {...register("cardNumber",{required:"Card Number is Required",minLength:{
-                value:16,
-                message:"Card Number length should be equal to 16."
-              },pattern:{
-                value:/^\d+$/,
-                message:"Card number must contain all the numbers."
-              }})}
-            />
-              <p style={{color:"yellow",fontSize:"10px"}}>{errors.cardNumber?.message}</p>
-          </div>
-          <div>
-            <label htmlFor="expiry">Expiry (MM/YY):</label>
-            <input
-              type="text"
-              id="expiry"
-              name="expiry"
-              placeholder="MM/YY"
-              {...register("expiry",{required:"Card Expiry is Required",pattern:{
+      <form
+        className={classes.payment_form}
+        onSubmit={handleSubmit(onSubmitHandler)}
+      >
+        <div>
+          <label htmlFor="name">Name: </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            {...register("name", {
+              required: "Name is Required",
+              pattern: {
+                value: /^[A-Za-z\s]*$/,
+                message: "Name must contain only the alphabatic letters.",
+              },
+              minLength: {
+                value: 4,
+                message: "Name must be at least 4 letters long]';/",
+              },
+            })}
+          />
+          <p style={{ color: "yellow", fontSize: "10px" }}>
+            {errors.name?.message}
+          </p>
+        </div>
+        <div>
+          <label htmlFor="cardNumber">Card Number: </label>
+          <input
+            type="text"
+            id="cardNumber"
+            name="cardNumber"
+            {...register("cardNumber", {
+              required: "Card Number is Required",
+              minLength: {
+                value: 16,
+                message: "Card Number length should be equal to 16.",
+              },
+              pattern: {
+                value: /^\d+$/,
+                message: "Card number must contain all the numbers.",
+              },
+            })}
+          />
+          <p style={{ color: "yellow", fontSize: "10px" }}>
+            {errors.cardNumber?.message}
+          </p>
+        </div>
+        <div>
+          <label htmlFor="expiry">Expiry (MM/YY): </label>
+          <input
+            type="text"
+            id="expiry"
+            name="expiry"
+            placeholder="MM/YY"
+            {...register("expiry", {
+              required: "Card Expiry is Required",
+              pattern: {
                 value: /^(0[1-9]|1[0-2])\/?([0-9]{4}|[0-9]{2})$/,
-                message:"Please specify the card expiry in MM/YY format."
-              }})}
-            />
-             <p style={{color:"yellow",fontSize:"10px"}}>{errors.expiry?.message}</p>
-          </div>
-          <div>
-            <label htmlFor="cvv">CVV:</label>
-            <input
-              type="password"
-              id="cvv"
-              name="cvv"
-              {...register("cvv",{required:"CVV is Required",minLength:{
-                value:3,
-                message:"CVV length should be equal to 3."
-              },pattern:{
-                value:/^\d+$/,
-                message:"CVV number must contain all the numbers."
-              }})}
-            />
-               <p style={{color:"yellow",fontSize:"10px"}}>{errors.cvv?.message}</p>
-          </div>
-          <div>
-            <label htmlFor="paymentMode">Payment Mode:</label>
-            <select
-              id="paymentMode"
-              name="paymentMode"
-              {...register("paymentMode")}
-            >
-              <option value="credit">Credit</option>
-              <option value="debit">Debit</option>
-              <option value="mastercard">Mastercard</option>
-            </select>
-          </div>
-          <button type="submit">Submit</button>
-        </form>
-      
-      
+                message: "Please specify the card expiry in MM/YY format.",
+              },
+            })}
+          />
+          <p style={{ color: "yellow", fontSize: "10px" }}>
+            {errors.expiry?.message}
+          </p>
+        </div>
+        <div>
+          <label htmlFor="cvv">CVV: </label>
+          <input
+            type="password"
+            id="cvv"
+            name="cvv"
+            {...register("cvv", {
+              required: "CVV is Required",
+              minLength: {
+                value: 3,
+                message: "CVV length should be equal to 3.",
+              },
+              pattern: {
+                value: /^\d+$/,
+                message: "CVV number must contain all the numbers.",
+              },
+            })}
+          />
+          <p style={{ color: "yellow", fontSize: "10px" }}>
+            {errors.cvv?.message}
+          </p>
+        </div>
+        <div>
+          <label htmlFor="paymentMode">Payment Mode: </label>
+          <select
+            id="paymentMode"
+            name="paymentMode"
+            {...register("paymentMode")}
+          >
+            <option value="credit">Credit</option>
+            <option value="debit">Debit</option>
+            <option value="mastercard">Mastercard</option>
+          </select>
+        </div>
+        <button className={classes.submit_button} type="submit">
+          Submit
+        </button>
+      </form>
     </React.Fragment>
   );
 };
--- src/components/Pages/Payment.js
+++ src/components/Pages/Payment.js
@@ -1,15 +1,14 @@
-import { Link } from "react-router-dom";
 import PaymentForm from "../Cart/PaymentForm";
+import classes from "./Payment.module.css";
 
 export default function Payment() {
   return (
     <div style={{ color: "red", marginTop: "100px" }}>
-      <div className="container">
+      <div className={classes.payment_container}>
         <div className="form-section">
           <PaymentForm />
         </div>
       </div>
-      <Link to="/ottomonMenu">Main Page</Link>
     </div>
   );
 }
