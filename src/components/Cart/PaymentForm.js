49efda12e32a8f9fc1bc8e328fa1d570d5921ff3
PriyaMJoseph <n01468981@humbermail.ca>
2023-03-06 13:29:48 -0500

    styling using bootstrap-initial commit

--- src/components/Cart/PaymentForm.js
+++ src/components/Cart/PaymentForm.js
@@ -7,7 +7,7 @@ import Alert from "@mui/material/Alert";
 import AlertTitle from "@mui/material/AlertTitle";
 import Stack from "@mui/material/Stack";
 import { useNavigate } from "react-router-dom";
-import classes from "./PaymentForm.module.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 const PaymentForm = (props) => {
   const addressCtx = useContext(AddressContext);
@@ -106,110 +106,125 @@ const PaymentForm = (props) => {
         </Stack>
       )}
 
-      <form
-        className={classes.payment_form}
-        onSubmit={handleSubmit(onSubmitHandler)}
-      >
-        <div>
-          <label htmlFor="name">Name: </label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            {...register("name", {
-              required: "Name is Required",
-              pattern: {
-                value: /^[A-Za-z\s]*$/,
-                message: "Name must contain only the alphabatic letters.",
-              },
-              minLength: {
-                value: 4,
-                message: "Name must be at least 4 letters long]';/",
-              },
-            })}
-          />
-          <p style={{ color: "yellow", fontSize: "10px" }}>
-            {errors.name?.message}
-          </p>
+      <form class="row gx-3" onSubmit={handleSubmit(onSubmitHandler)}>
+        <div class="col-12">
+          <div class="d-flex flex-column">
+            <p class="text mb-1 fw-bold">Name: </p>
+            <input
+              class="form-control mb-3 w-75"
+              type="text"
+              id="name"
+              name="name"
+              {...register("name", {
+                required: "Name is Required",
+                pattern: {
+                  value: /^[A-Za-z\s]*$/,
+                  message: "Name must contain only the alphabatic letters.",
+                },
+                minLength: {
+                  value: 4,
+                  message: "Name must be at least 4 letters long]';/",
+                },
+              })}
+            />
+            <p style={{ color: "yellow", fontSize: "10px" }}>
+              {errors.name?.message}
+            </p>
+          </div>
         </div>
-        <div>
-          <label htmlFor="cardNumber">Card Number: </label>
-          <input
-            type="text"
-            id="cardNumber"
-            name="cardNumber"
-            {...register("cardNumber", {
-              required: "Card Number is Required",
-              minLength: {
-                value: 16,
-                message: "Card Number length should be equal to 16.",
-              },
-              pattern: {
-                value: /^\d+$/,
-                message: "Card number must contain all the numbers.",
-              },
-            })}
-          />
-          <p style={{ color: "yellow", fontSize: "10px" }}>
-            {errors.cardNumber?.message}
-          </p>
+        <div class="col-12">
+          <div class="d-flex flex-column">
+            <p class="text mb-1 fw-bold">Card Number: </p>
+            <input
+              class="form-control mb-3 w-50"
+              type="text"
+              id="cardNumber"
+              name="cardNumber"
+              {...register("cardNumber", {
+                required: "Card Number is Required",
+                minLength: {
+                  value: 16,
+                  message: "Card Number length should be equal to 16.",
+                },
+                pattern: {
+                  value: /^\d+$/,
+                  message: "Card number must contain all the numbers.",
+                },
+              })}
+            />
+            <p style={{ color: "yellow", fontSize: "10px" }}>
+              {errors.cardNumber?.message}
+            </p>
+          </div>
         </div>
-        <div>
-          <label htmlFor="expiry">Expiry (MM/YY): </label>
-          <input
-            type="text"
-            id="expiry"
-            name="expiry"
-            placeholder="MM/YY"
-            {...register("expiry", {
-              required: "Card Expiry is Required",
-              pattern: {
-                value: /^(0[1-9]|1[0-2])\/?([0-9]{4}|[0-9]{2})$/,
-                message: "Please specify the card expiry in MM/YY format.",
-              },
-            })}
-          />
-          <p style={{ color: "yellow", fontSize: "10px" }}>
-            {errors.expiry?.message}
-          </p>
+        <div class="col-12">
+          <div class="d-flex flex-column">
+            <p class="text mb-1 fw-bold">Expiry (MM/YY): </p>
+            <input
+              class="form-control mb-3 w-25"
+              type="text"
+              id="expiry"
+              name="expiry"
+              placeholder="MM/YY"
+              {...register("expiry", {
+                required: "Card Expiry is Required",
+                pattern: {
+                  value: /^(0[1-9]|1[0-2])\/?([0-9]{4}|[0-9]{2})$/,
+                  message: "Please specify the card expiry in MM/YY format.",
+                },
+              })}
+            />
+            <p style={{ color: "yellow", fontSize: "10px" }}>
+              {errors.expiry?.message}
+            </p>
+          </div>
         </div>
-        <div>
-          <label htmlFor="cvv">CVV: </label>
-          <input
-            type="password"
-            id="cvv"
-            name="cvv"
-            {...register("cvv", {
-              required: "CVV is Required",
-              minLength: {
-                value: 3,
-                message: "CVV length should be equal to 3.",
-              },
-              pattern: {
-                value: /^\d+$/,
-                message: "CVV number must contain all the numbers.",
-              },
-            })}
-          />
-          <p style={{ color: "yellow", fontSize: "10px" }}>
-            {errors.cvv?.message}
-          </p>
+        <div class="col-12">
+          <div class="d-flex flex-column">
+            <p class="text mb-1 fw-bold">CVV: </p>
+            <input
+              class="form-control mb-3 w-25"
+              type="password"
+              id="cvv"
+              name="cvv"
+              placeholder="***"
+              {...register("cvv", {
+                required: "CVV is Required",
+                minLength: {
+                  value: 3,
+                  message: "CVV length should be equal to 3.",
+                },
+                pattern: {
+                  value: /^\d+$/,
+                  message: "CVV number must contain all the numbers.",
+                },
+              })}
+            />
+            <p style={{ color: "yellow", fontSize: "10px" }}>
+              {errors.cvv?.message}
+            </p>
+          </div>
         </div>
-        <div>
-          <label htmlFor="paymentMode">Payment Mode: </label>
-          <select
-            id="paymentMode"
-            name="paymentMode"
-            {...register("paymentMode")}
-          >
-            <option value="credit">Credit</option>
-            <option value="debit">Debit</option>
-            <option value="mastercard">Mastercard</option>
-          </select>
+        <div class="col-12">
+          <div class="d-flex flex-column">
+            <p class="text mb-1 fw-bold">Payment Mode: </p>
+            <select
+              class="w-50 mb-3"
+              id="paymentMode"
+              name="paymentMode"
+              {...register("paymentMode")}
+            >
+              <option value="credit">Credit</option>
+              <option value="debit">Debit</option>
+              <option value="mastercard">Mastercard</option>
+            </select>
+          </div>
         </div>
-        <button className={classes.submit_button} type="submit">
-          Submit
-        </button>
+        <div class="col-12">
+          <button class="btn btn-primary btn-lg raised mb-3" type="submit">
+            Submit
+          </button>
+        </div>
       </form>
     </React.Fragment>
   );
--- src/components/Pages/Payment.js
+++ src/components/Pages/Payment.js
@@ -1,13 +1,13 @@
 import PaymentForm from "../Cart/PaymentForm";
+import "bootstrap/dist/css/bootstrap.css";
 import classes from "./Payment.module.css";
 
 export default function Payment() {
   return (
-    <div style={{ color: "red", marginTop: "100px" }}>
-      <div className={classes.payment_container}>
-        <div className="form-section">
-          <PaymentForm />
-        </div>
+    <div className={`container p-0 body w-50 title ${classes.title}`}>
+      <div class="card px-4 d-flex justify-content-center">
+        <p class="h6 py-3 m-auto fs-2 fw-bolder">Payment Details</p>
+        <PaymentForm />
       </div>
     </div>
   );
