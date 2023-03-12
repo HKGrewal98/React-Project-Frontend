847268545ef55d6668f702d08e178c73a4142866
HKGrewal98 <104564558+HKGrewal98@users.noreply.github.com>
2023-03-12 13:56:11 -0400

    Removed Jobs Page.

--- src/components/UI/MainNavigation.js
+++ src/components/UI/MainNavigation.js
@@ -10,7 +10,6 @@ import { NavLink } from 'react-router-dom'
                     <li><NavLink to="/ottomonMenu"  className={(isActive)=> isActive ? classes.active : undefined} end>Menu</NavLink></li>
                     <li><NavLink to="/ottomons" className={(isActive)=> isActive ? classes.active : undefined}>About</NavLink></li>
                     <li><NavLink to="/ottomons/reviews" className={(isActive)=> isActive ? classes.active : undefined}>Reviews</NavLink></li>
-                    <li><NavLink to="/ottomons/jobs" className={(isActive)=> isActive ? classes.active : undefined}>Jobs</NavLink></li>
                     <li><NavLink to="/ottomons/contactus" className={(isActive)=> isActive ? classes.active : undefined}>Contact Us</NavLink></li>
                 </ul>
             </nav>
